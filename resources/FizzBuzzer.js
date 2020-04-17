cf54f78c146d4ce297cc091689c83836d72dec20
TyVoRu <vonruden.tyler.von@gmail.com>
2020-04-16 20:38:00 -0500

    Implemented Fizz Buzz

--- resources/FizzBuzzer.js
+++ resources/FizzBuzzer.js
@@ -1,3 +1,23 @@
 ﻿function FizzBuzzer(input) {
-    return null;
+    let result = "";
+    if (input % 3 === 0) {
+        result += "Fizz";
+    }
+    if (input % 5 === 0) {
+        result += "Buzz";
+    }
+    return result === "" ? input : result;
+};
+
+function FizzBuzzerV1(input) {
+    if (input % 15 === 0) {
+        return "FizzBuzz";
+    }
+    if (input % 5 === 0) {
+        return "Buzz";
+    }
+    if (input % 3 === 0) {
+        return "Fizz";
+    }
+    return input;
 };
